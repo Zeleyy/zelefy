72a7451aea769248dfb6c93d0d6a25d5f8621bb6
Zeleyy <zeleyy.dev@gmail.com>
2026-09-09 18:52:16 +0300

    feat: add queries for profiles and follows

---
+++ apps/profiles/src/db/repository/follows.rs
@@ -0,0 +1,48 @@
+use sqlx::PgExecutor;
+
+use crate::models::follows::{CreateFollowDto, DeleteFollowDto, Follow};
+
+pub async fn create<'e, E>(
+    executor: E,
+    params: CreateFollowDto,
+) -> Result<Follow, sqlx::Error>
+where
+    E: PgExecutor<'e>,
+{
+    sqlx::query_as!(
+        Follow,
+        r#"
+            INSERT INTO follows (follower_id, followed_id)
+            VALUES ($1, $2)
+            RETURNING 
+                follower_id
+                , followed_id
+                , created_at
+        "#,
+        params.follower_id,
+        params.followed_id
+    )
+    .fetch_one(executor)
+    .await
+}
+
+pub async fn delete<'e, E>(
+    executor: E,
+    params: DeleteFollowDto,
+) -> Result<bool, sqlx::Error>
+where
+    E: PgExecutor<'e>,
+{
+    let result = sqlx::query!(
+        r#"
+            DELETE FROM follows
+            WHERE follower_id = $1 AND followed_id = $2
+        "#,
+        params.follower_id,
+        params.followed_id
+    )
+    .execute(executor)
+    .await?;
+
+    Ok(result.rows_affected() > 0)
+}
