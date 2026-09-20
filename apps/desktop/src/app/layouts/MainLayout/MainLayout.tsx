20644ae3cc448328ea251d4c7c0e5834456d04c5
Zeleyy <zeleyy.dev@gmail.com>
2026-09-20 18:11:54 +0300

    fix: rename titlebar

--- apps/desktop/src/app/layouts/MainLayout/MainLayout.tsx
+++ apps/desktop/src/app/layouts/MainLayout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import styles from "./MainLayout.module.scss";
 import { Outlet } from "react-router-dom";
-import { Titlebar } from "@/widgets/Titlebar";
+import { Titlebar } from "@/widgets/Titlebar_";
 import { Sidebar } from "@/widgets/Sidebar";
 
 export const MainLayout = () => {
